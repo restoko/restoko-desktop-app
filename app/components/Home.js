79a1f469f666e15bb0e798b2ad5e80beca180d72
Alleo Indong <freedom_war550@live.com.ph>
2016-01-17 00:30:28 +0800

    added materialize lite

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './Home.module.css';
 
+import Login from './partials/Login';
+
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <div className={styles.container}>
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
+            <Login />
         </div>
       </div>
     );
---
+++ app/components/partials/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import styles from './Login.module.css';
+
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div>
+                <div className={styles.container}>
+                    <form method="POST">
+                        
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
