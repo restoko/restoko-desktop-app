7d37337e83fe557404e58f26ad2274bcef0c2327
Alleo Indong <freedom_war550@live.com.ph>
2016-01-17 01:01:28 +0800

    menu fix

--- app/components/partials/SidebarMenu.js
+++ app/components/partials/SidebarMenu.js
@@ -1,30 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-
 export default class Login extends Component {
     render() {
         return (
             <div className="mdl-layout mdl-js-layout">
                 <header className="mdl-layout__header mdl-layout__header--transparent">
                     <div className="mdl-layout__header-row">
-                        <span className="mdl-layout-title">Title</span>
+                        <span className="mdl-layout-title">Javalogy</span>
                         <div className="mdl-layout-spacer"></div>
                         <nav className="mdl-navigation">
-                            <a className="mdl-navigation__link" href="">Link</a>
-                            <a className="mdl-navigation__link" href="">Link</a>
-                            <a className="mdl-navigation__link" href="">Link</a>
-                            <a className="mdl-navigation__link" href="">Link</a>
+                            <Link to="/" className="mdl-navigation__link">Home</Link>
+                            <Link to="/counter" className="mdl-navigation__link">Categories</Link>
+                            <Link to="/" className="mdl-navigation__link">Products</Link>
+                            <Link to="/" className="mdl-navigation__link">POS</Link>
+                            <Link to="/" className="mdl-navigation__link">Kitchen</Link>
+                            <Link to="/" className="mdl-navigation__link">Reports</Link>
+                            <Link to="/" className="mdl-navigation__link">Users</Link>
                         </nav>
                     </div>
                 </header>
                 <div className="mdl-layout__drawer">
-                    <span className="mdl-layout-title">Title</span>
+                    <span className="mdl-layout-title">Javalogy</span>
                     <nav className="mdl-navigation">
-                        <a className="mdl-navigation__link" href="">Link</a>
-                        <a className="mdl-navigation__link" href="">Link</a>
-                        <a className="mdl-navigation__link" href="">Link</a>
-                        <a className="mdl-navigation__link" href="">Link</a>
+                        <Link to="/" className="mdl-navigation__link">Categories</Link>
+                        <Link to="/" className="mdl-navigation__link">Products</Link>
+                        <Link to="/" className="mdl-navigation__link">POS</Link>
+                        <Link to="/" className="mdl-navigation__link">Kitchen</Link>
+                        <Link to="/" className="mdl-navigation__link">Reports</Link>
+                        <Link to="/" className="mdl-navigation__link">Users</Link>
                     </nav>
                 </div>
                 <main className="mdl-layout__content">
