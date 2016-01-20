45812439c44549d02151be4df227140088293f21
Alleo Indong <freedom_war550@live.com.ph>
2016-01-20 19:23:20 +0800

    action for tables

---
+++ app/actions/table.js
@@ -0,0 +1,74 @@
+import axios from 'axios';
+import { API_URL } from '../api/apiconfig';
+
+export const GET_TABLES = 'GET_TABLES';
+export const RECV_TABLES_DATA = 'RECV_TABLES_DATA';
+export const RECV_TABLES_ERROR = 'RECV_TABLES_ERROR';
+export const CREATE_TABLE = 'CREATE_TABLE';
+
+export function getTables() {
+    return {
+        type: GET_TABLES
+    };
+}
+
+function createTable() {
+    return {
+        type: CREATE_TABLE
+    };
+}
+
+function receiveData(json) {
+    return{
+        type: RECV_TABLES_DATA,
+        data: json
+    }
+};
+
+function receiveError(json) {
+    return {
+        type: RECV_TABLES_ERROR,
+        data: json
+    }
+};
+
+export function fetchTables() {
+    return function(dispatch) {
+        dispatch(getTables());
+
+        return axios({
+            url: API_URL + 'products',
+            timeout: 20000,
+            method: 'get',
+            responseType: 'json'
+        })
+            .then(function(response) {
+                dispatch(receiveData(response.data.data));
+            })
+            .catch(function(response){
+                dispatch(receiveError(response.data.data));
+                dispatch(pushState(null,'/error'));
+            });
+    }
+}
+
+export function createTableRequest(data) {
+    return function(dispatch) {
+        dispatch(createTable());
+
+        return axios({
+            url: API_URL + 'products',
+            timeout: 20000,
+            method: 'post',
+            responseType: 'json',
+            data: data
+        })
+            .then(function(response) {
+                dispatch(fetchTables());
+            })
+            .catch(function(response){
+                dispatch(receiveError(response.data.data));
+                dispatch(pushState(null,'/error'));
+            });
+    }
+}
