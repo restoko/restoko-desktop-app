e13530c7284d2b4fa4142123b6acbe5493b21c59
Alleo Indong <freedom_war550@live.com.ph>
2016-01-20 15:38:37 +0800

    listing of product and product form

---
+++ app/components/products/Form.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import NotificationSystem from 'react-notification-system';
+
+export default class Form extends Component {
+    state = {
+        name: null,
+        change: null
+    };
+
+    notificationSystem = null;
+
+    componentDidMount() {
+        this.notificationSystem = this.refs.notificationSystem;
+    }
+
+    clickHandler = () => {
+        let data = {
+            name: this.state.name,
+            description: this.state.description
+        };
+
+        if (this.validateData()) {
+            return false;
+        }
+
+        this.props.createProduct(data);
+    };
+
+    validateData = () => {
+        let errorFound = false;
+        let message = null;
+
+        if (this.state.name == null) {
+            message = 'Field Name is required<br/>';
+
+            errorFound = true;
+        }
+
+        if (this.state.description == null) {
+            message += 'Field Description is required';
+
+            errorFound = true;
+        }
+
+        if (errorFound) {
+            this.notificationSystem.addNotification({
+                title: 'Opps! Something went wrong',
+                message: message,
+                level: 'error',
+                position: 'bl'
+            });
+        }
+
+        return errorFound;
+    };
+
+    handleNameChange = (e) => {
+        this.setState({ name: e.target.value });
+    };
+
+    handleDescriptionChange = (e) => {
+        this.setState({ description: e.target.value });
+    };
+
+    render() {
+        return (
+            <div>
+                <NotificationSystem ref="notificationSystem" allowHTML={true} />
+
+                <form action="#">
+                    <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                        <input
+                            className="mdl-textfield__input"
+                            type="text" id="name"
+                            name="name" pattern="[A-Z,a-z, ]*"
+                            onChange={this.handleNameChange} />
+
+                        <label className="mdl-textfield__label" htmlFor="name">Name</label>
+                        <span className="mdl-textfield__error">Letters and spaces only</span>
+                    </div>
+
+                    <br/>
+
+                    <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                        <input className="mdl-textfield__input"
+                               type="text" id="description" name="description"
+                               pattern="[A-Z,a-z, ]*"
+                               onChange={this.handleDescriptionChange} />
+
+                        <label className="mdl-textfield__label" htmlFor="description">Description</label>
+                        <span className="mdl-textfield__error">Letters and spaces only</span>
+                    </div>
+
+                    <button
+                        type="button"
+                        className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
+                        onClick={this.clickHandler}>
+
+                        Save Category
+                    </button>
+                </form>
+            </div>
+        );
+    };
+}
--- app/components/products/Product.js
+++ app/components/products/Product.js
@@ -1,23 +1,52 @@
 import React, { Component } from 'react';
 import ProductCollection from './ProductCollection';
+import PageListHeader from '../commons/PageListHeader';
+import PageFormHeader from '../commons/PageFormHeader';
+import FormContainer from '../commons/FormContainer';
+import ProductCreateForm from './Form';
 
 export default class Product extends Component {
+    state = {
+        showCreateForm: false
+    };
 
     componentDidMount() {
         this.props.fetchProducts();
     };
 
+    createCategory = (data) => {
+        this.setState({ showCreateForm: false });
+        this.props.createCategoryRequest(data);
+    };
+
+    onCreateClick = () => {
+        this.setState({ showCreateForm: true });
+    };
+
+    onBackClick = () => {
+        this.setState({ showCreateForm: false });
+    };
+
     render() {
         const { products } = this.props;
 
+        if (! this.state.showCreateForm) {
+            return (
+                <div>
+                    <PageListHeader title="Products" createButtonTitle="Create New Product" onCreateClick={this.onCreateClick}/>
+                    <ProductCollection products={products.data} />
+                </div>
+            )
+        }
+
         return (
             <div>
-                <ProductCollection products={products.data} />
+                <PageFormHeader title="Create a product" backButtonTitle="Back to Products" onBackClick={this.onBackClick}/>
+                <FormContainer>
+                    <ProductCreateForm createProduct={this.createProduct} />
+                </FormContainer>
 
-                <button className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored" style={{ position: 'fixed', bottom: '20px', right: '20px'}}>
-                    <i className="material-icons">add</i>
-                </button>
             </div>
-        )
+        );
     }
 }
