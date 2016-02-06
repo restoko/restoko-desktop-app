c6989e2931a5909565986fbcdf6265b6717b5a3a
Alleo Indong <freedom_war550@live.com.ph>
2016-02-07 07:53:02 +0800

    dialog

---
+++ app/components/pos/Payment.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Dialog from '../commons/Dialog';
+
+export default class Payment extends Component {
+    render() {
+        return (
+            <Dialog title="Checkout">
+                <p>Hello world</p>
+            </Dialog>
+        )
+    }
+}
