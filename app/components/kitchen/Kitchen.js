af8984d43fa4a947187e53a010134efe9207184a
Alleo Indong <freedom_war550@live.com.ph>
2016-02-07 11:11:02 +0800

    testing kitchen page

--- app/components/kitchen/Kitchen.js
+++ app/components/kitchen/Kitchen.js
@@ -1,7 +1,40 @@
 import React, { Component } from 'react';
 import ProductCard from './ProductCard';
+import { API_URL } from '../../api/apiconfig';
+import axios from 'axios';
 
 export default class Kitchen extends Component {
+    state = {
+        pendingOrders: []
+    };
 
+    getPendingOrders = () => {
+        axios({
+            url: API_URL + 'kitchen/',
+            timeout: 20000,
+            method: 'get',
+            responseType: 'json'
+        })
+        .then((response) => {
+            this.setState({ pendingOrders: response.data.data});
+            console.log(response.data.data);
+        })
+        .catch(function(response){
+            console.log(response);
+            console.log('ERROR ON GETTING CART');
+        });
+    };
+
+    componentDidMount() {
+        this.getPendingOrders();
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
 }
 
