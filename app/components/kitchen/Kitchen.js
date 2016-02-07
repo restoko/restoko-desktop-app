77fcbd0053cec6e88c798b7c332c867b4d28d7fd
Alleo Indong <freedom_war550@live.com.ph>
2016-02-07 11:51:33 +0800

    added product card for the kitchen

--- app/components/kitchen/Kitchen.js
+++ app/components/kitchen/Kitchen.js
@@ -17,12 +17,40 @@ export default class Kitchen extends Component {
         })
         .then((response) => {
             this.setState({ pendingOrders: response.data.data});
-            console.log(response.data.data);
         })
         .catch(function(response){
             console.log(response);
             console.log('ERROR ON GETTING CART');
         });
+    };
+
+
+    cancelOrder = (orderId) => {
+        let id = orderId;
+        let data = this.state.pendingOrders;
+
+        for(var i = 0; i < data.length; i++) {
+            if(data[i].cart_item_id == id) {
+                data.splice(i, 1);
+                break;
+            }
+        }
+
+        this.setState({ pendingOrders: data });
+    };
+
+    readyOrder = (orderId) => {
+        let id = orderId;
+        let data = this.state.pendingOrders;
+
+        for(var i = 0; i < data.length; i++) {
+            if(data[i].cart_item_id == id) {
+                data.splice(i, 1);
+                break;
+            }
+        }
+
+        this.setState({ pendingOrders: data });
     };
 
     componentDidMount() {
@@ -32,7 +60,17 @@ export default class Kitchen extends Component {
     render() {
         return (
             <div>
-
+                {this.state.pendingOrders.map(function(order, i) {
+                    return <ProductCard
+                        status={order.status}
+                        id={order.cart_item_id}
+                        productName={order.product_name}
+                        tableName={order.table}
+                        quantity={order.quantity}
+                        key={i}
+                        readyOrder={this.readyOrder}
+                        cancelOrder={this.cancelOrder} />
+                }.bind(this))}
             </div>
         )
     }
--- app/components/kitchen/ProductCard.js
+++ app/components/kitchen/ProductCard.js
@@ -1,6 +1,42 @@
 import React, { Component } from 'react';
+import Card from 'material-ui/lib/card/card';
+import CardActions from 'material-ui/lib/card/card-actions';
+import CardHeader from 'material-ui/lib/card/card-header';
+import CardTitle from 'material-ui/lib/card/card-title';
+import FlatButton from 'material-ui/lib/flat-button';
+import CardText from 'material-ui/lib/card/card-text';
 
 export default class ProductCard extends Component {
 
+    handleCancelled = (e) => {
+        this.props.cancelOrder(this.props.id);
+    };
+
+    handleReady = (e) => {
+        this.props.readyOrder(this.props.id);
+    };
+
+    render() {
+        const { tableName, productName, status, id, quantity } = this.props;
+
+        return (
+            <Card style={{width: '30%', float: 'left', margin: '5px 10px 5px 10px'}}>
+                <CardTitle
+                    title={tableName.toUpperCase()}
+                    subtitle={status.toUpperCase()}
+                    style={{backgroundColor: '#51A527'}}
+                />
+
+                <CardText>
+                    {quantity}x of  <strong>{productName.toUpperCase()}</strong>
+                </CardText>
+
+                <CardActions style={{float: 'right'}}>
+                    <FlatButton label="Cancelled" onClick={this.handleCancelled} />
+                    <FlatButton label="Ready" primary={true} onClick={this.handleReady} />
+                </CardActions>
+            </Card>
+        )
+    }
 }
 
